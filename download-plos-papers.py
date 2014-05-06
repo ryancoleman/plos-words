fad0ea1a0eae65ea23eea113cfd9f371c9380d15
Ryan G. Coleman <ryan.g.coleman@gmail.com>
2014-05-06 07:29:46 -0700

    adding lots of different possible url & socket errors, etc

--- download-plos-papers.py
+++ download-plos-papers.py
@@ -11,6 +11,7 @@ import sys  # always, pretty much
 import string  # string handling
 import urllib2  # url handling
 import urllib  # url encoding
+import socket  # for error catching
 import os
 import argparse
 import bz2
@@ -23,7 +24,7 @@ def plosOneQuery(articleNumber):
   request = urllib2.Request(
       url='http://www.plosone.org/article/fetchObjectAttachment.action', 
       data=articleEncode)
-  result = urllib2.urlopen(request)
+  result = urllib2.urlopen(request, timeout=5.0)  # timeout can be changed
   dataOut = result.read()
   return dataOut
 
@@ -31,8 +32,8 @@ try:
   os.mkdir('xml')
 except OSError:   # directory exists
   pass  # which is fine
-for number in xrange(10000):  # eventually change to 100000 and get them all
-  artNumber = string.zfill(number, 7)
+for number in xrange(100000):  # eventually raise once plos adds more papers
+  artNumber = string.zfill(number, 7)   # hard coded 7 digit number, won't last
   if not os.path.exists(os.path.join('xml', artNumber + '.xml.bz2')):
     if not os.path.exists(os.path.join('xml', artNumber + '.xml')):
       try:
@@ -40,5 +41,13 @@ for number in xrange(10000):  # eventually change to 100000 and get them all
         outxml = bz2.BZ2File(os.path.join('xml', artNumber + '.xml.bz2'), 'wb')
         outxml.write(data)
         outxml.close()
+      except socket.timeout:
+        print artNumber, 'timeout error'
+      except socket.error:
+        print artNumber, 'connection reset by peer error'
       except urllib2.HTTPError:
-        print artNumber + ' failed to download'
+        print artNumber, 'failed to download'
+      except urllib2.URLError:
+        print artNumber, 'url open error'
+      except httplib.BadStatusLine:
+        print artNumebr, ' bad status line'
