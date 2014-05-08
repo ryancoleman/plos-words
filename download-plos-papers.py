7515b7f78fcadff982cbddd4a39856f6718f57f5
Ryan G. Coleman <ryan.g.coleman@gmail.com>
2014-05-08 00:01:18 -0700

    wrapping the script to contain errors and amke it easier to retry a few times, which almost totally eliminates problems except 404 errors

--- download-plos-papers.py
+++ download-plos-papers.py
@@ -3,6 +3,7 @@
 #run this script at your own risk. i can't find anything about how many
 # articles PLOS would like you to limit yourself to grabbing at once,
 # but if they have a limit, this script will break it.
+# currently, this downloads about a thousand files an hour. YMMV
 
 #http://www.plosone.org/article/fetchObjectAttachment.action?
 # uri=info%3Adoi%2F10.1371%2Fjournal.pone.0075992&representation=XML
@@ -29,6 +30,31 @@ def plosOneQuery(articleNumber):
   dataOut = result.read()
   return dataOut
 
+def downloadAndSaveWrapper(articleNumber, filename, attempts=5):
+  attempt, doneYet = 0, False
+  while attempt < attempts and not doneYet:
+    attempt += 1
+    try:
+      data = plosOneQuery(artNumber)
+      if len(data) > 0:
+        outxml = bz2.BZ2File(filename, 'wb')
+        outxml.write(data)
+        outxml.close()
+        doneYet = True  # can quit early
+      else:
+        print artNumber, 'got size 0 xml file', attempt
+    except socket.timeout:
+      print artNumber, 'timeout error', attempt
+    except socket.error:
+      print artNumber, 'connection reset by peer error', attempt
+    except urllib2.HTTPError:  # 404, doesn't exist, won't exist
+      print artNumber, 'failed to download', attempt
+      doneYet = True
+    except urllib2.URLError:
+      print artNumber, 'url open error', attempt
+    except httplib.BadStatusLine:
+      print artNumber, ' bad status line', attempt
+
 try:
   os.mkdir('xml')
 except OSError:   # directory exists
@@ -37,18 +63,5 @@ for number in xrange(100000):  # eventually raise once plos adds more papers
   artNumber = string.zfill(number, 7)   # hard coded 7 digit number, won't last
   if not os.path.exists(os.path.join('xml', artNumber + '.xml.bz2')):
     if not os.path.exists(os.path.join('xml', artNumber + '.xml')):
-      try:
-        data = plosOneQuery(artNumber)
-        outxml = bz2.BZ2File(os.path.join('xml', artNumber + '.xml.bz2'), 'wb')
-        outxml.write(data)
-        outxml.close()
-      except socket.timeout:
-        print artNumber, 'timeout error'
-      except socket.error:
-        print artNumber, 'connection reset by peer error'
-      except urllib2.HTTPError:
-        print artNumber, 'failed to download'
-      except urllib2.URLError:
-        print artNumber, 'url open error'
-      except httplib.BadStatusLine:
-        print artNumber, ' bad status line'
+      outxmlbz2name = os.path.join('xml', artNumber + '.xml.bz2')
+      downloadAndSaveWrapper(artNumber, outxmlbz2name)
